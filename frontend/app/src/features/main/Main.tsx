e40cd2f5add3203fe77508903f3c4f1f3f4210df
goppy <tacumin.goppy.06@gmail.com>
2023-03-15 23:07:18 +0900

    サイドバーの追加に合わせてmainを調整’ ’

--- frontend/app/src/features/main/Main.tsx
+++ frontend/app/src/features/main/Main.tsx
@@ -4,9 +4,10 @@ export default function Main({ children }: { children: React.ReactNode }) {
   return (
     <Container
       as="main"
-      maxW="container.lg"
-      my="4"
-      minH="calc(100vh - 115px - 2rem)"
+      maxW="5xl"
+      px={4}
+      py={8}
+      marginTop="60px"
     >
       {children}
     </Container>
