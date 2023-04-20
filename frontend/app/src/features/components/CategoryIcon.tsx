ac64671285dfd7e12d6276be55ecb895fb48bcce
goppy <tacumin.goppy.06@gmail.com>
2023-04-20 11:53:39 +0900

    アイコン背景の配色を明るめにした

--- frontend/app/src/features/components/CategoryIcon.tsx
+++ frontend/app/src/features/components/CategoryIcon.tsx
@@ -24,26 +24,25 @@ type CategoryIcons = {
 }
 
 const categoryIcons: CategoryIcons = {
-  "IT": { icon: <FaLaptopCode style={{ width: 60, height: 60 }} />, bg: "purple.500" },
-  "株・投資": { icon: <FaChartBar style={{ width: 60, height: 60 }}/>, bg: "teal.500" },
-  "エクセル・スプレッドシート": { icon: <FaFileExcel style={{ width: 60, height: 60 }} />, bg: "yellow.500" },
-  "パワーポイント・スライド": { icon: <FaFilePowerpoint style={{ width: 60, height: 60 }} />, bg: "orange.500" },
-  "ワード・ドキュメント": { icon: <FaFileWord style={{ width: 60, height: 60 }} />, bg: "blue.500" },
-  "メール": { icon: <FaEnvelope style={{ width: 60, height: 60 }} />, bg: "green.500" },
-  "文書": { icon: <FaRegFileAlt style={{ width: 60, height: 60 }} />, bg: "gray.500" },
-  "コミュニケーション": { icon: <FaRegComments style={{ width: 60, height: 60 }} />, bg: "pink.500" },
-  "会計・経理・税務": { icon: <FaCalculator style={{ width: 60, height: 60 }} />, bg: "red.500" },
-  "効率・時短": { icon: <FaBusinessTime style={{ width: 60, height: 60 }} />, bg: "indigo.500" },
-  "マネジメント": { icon: <FaUsers style={{ width: 60, height: 60 }} />, bg: "cyan.500" },
-  "法務": { icon: <FaBalanceScaleRight style={{ width: 60, height: 60 }} />, bg: "gray.500" },
-  "教育": { icon: <FaGraduationCap style={{ width: 60, height: 60 }} />, bg: "purple.500" },
-  "マーケティング": { icon: <FaClipboardCheck style={{ width: 60, height: 60 }} />, bg: "teal.500" },
-  "時事": { icon: <FaNewspaper style={{ width: 60, height: 60 }} />, bg: "orange.500" },
-  "その他": { icon: <FaQuestion style={{ width: 60, height: 60 }} />, bg: "gray.500" }
+  "IT": { icon: <FaLaptopCode style={{ width: 60, height: 60 }} />, bg: "purple.300" },
+  "株・投資": { icon: <FaChartBar style={{ width: 60, height: 60 }}/>, bg: "teal.300" },
+  "エクセル・スプレッドシート": { icon: <FaFileExcel style={{ width: 60, height: 60 }} />, bg: "yellow.300" },
+  "パワーポイント・スライド": { icon: <FaFilePowerpoint style={{ width: 60, height: 60 }} />, bg: "orange.300" },
+  "ワード・ドキュメント": { icon: <FaFileWord style={{ width: 60, height: 60 }} />, bg: "blue.300" },
+  "メール": { icon: <FaEnvelope style={{ width: 60, height: 60 }} />, bg: "green.300" },
+  "文書": { icon: <FaRegFileAlt style={{ width: 60, height: 60 }} />, bg: "gray.300" },
+  "コミュニケーション": { icon: <FaRegComments style={{ width: 60, height: 60 }} />, bg: "pink.300" },
+  "会計・経理・税務": { icon: <FaCalculator style={{ width: 60, height: 60 }} />, bg: "red.300" },
+  "効率・時短": { icon: <FaBusinessTime style={{ width: 60, height: 60 }} />, bg: "indigo.300" },
+  "マネジメント": { icon: <FaUsers style={{ width: 60, height: 60 }} />, bg: "cyan.300" },
+  "法務": { icon: <FaBalanceScaleRight style={{ width: 60, height: 60 }} />, bg: "gray.300" },
+  "教育": { icon: <FaGraduationCap style={{ width: 60, height: 60 }} />, bg: "purple.300" },
+  "マーケティング": { icon: <FaClipboardCheck style={{ width: 60, height: 60 }} />, bg: "teal.300" },
+  "時事": { icon: <FaNewspaper style={{ width: 60, height: 60 }} />, bg: "orange.300" },
+  "その他": { icon: <FaQuestion style={{ width: 60, height: 60 }} />, bg: "gray.300" }
 };
-
 export default function CategoryIcon({ category }: { category: string }): JSX.Element {
-  const { icon, bg } = categoryIcons[category] ?? { icon: <Icon />, bg: "gray.500" };
+  const { icon, bg } = categoryIcons[category] ?? { icon: <Icon />, bg: "gray.300" };
   // カテゴリに対応するアイコンと背景色が存在しない場合は、デフォルトのアイコンと色を使用する
   return (
     <Box
