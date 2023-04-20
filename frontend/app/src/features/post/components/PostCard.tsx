31bf8ab91fdffc5ed27401cb6323e52668ea2920
goppy <tacumin.goppy.06@gmail.com>
2023-04-21 03:41:56 +0900

    fix

--- frontend/app/src/features/post/components/PostCard.tsx
+++ frontend/app/src/features/post/components/PostCard.tsx
@@ -3,7 +3,9 @@
 import {
   Box,
   Flex,
-  Heading
+  Heading,
+  Grid,
+  GridItem
 } from "@/features/components";
 import {
   FaHeart,
@@ -21,66 +23,85 @@ type PostCardProps = {
 export default function PostCard({ post }: PostCardProps) {
 
   return (
-    <Box borderWidth="1px" borderRadius="lg" overflow="hidden" margin={5} width="350px" boxShadow="xl">
+    <Box borderWidth="1px" borderRadius="lg" overflow="hidden" w="100%" boxShadow="xl">
       <Box as="a" href={`/posts/${post.id}`} cursor="pointer">
-        <Box position="relative" width="100%" height="120px">
-          <CategoryIcon category={post.category} />
-        </Box>
-        <Box p="6">
-          <Flex alignItems="baseline" mb={2}>
-            <CategoryTag category={post.category} />
-          </Flex>
-
-          <Box
-            mt="2"
-            overflow="hidden"
-            display="-webkit-box"
-            style={{
-              WebkitLineClamp: 1,
-              WebkitBoxOrient: "vertical"
-            }}
-            height="20px"
-          >
-            <Heading size="md" isTruncated height="20px">
-              {post.title}
-            </Heading>
-          </Box>
-
-          <Box
-            mt="2"
-            color="gray.600"
-            overflow="hidden"
-            display="-webkit-box"
-            style={{
-              WebkitLineClamp: 2,
-              WebkitBoxOrient: "vertical"
-            }}
-            height="50px"
-          >
-            {post.description}
-          </Box>
-
-          <Flex mt={4} justifyContent="space-between" alignItems="center">
+        <Grid
+          templateAreas={`
+            "categoryIcon categoryIcon categoryIcon"
+            "category category category"
+            "title title title"
+            "description description description"
+            "read like views"
+          `}
+          templateRows="120px 1fr 20px 70px 1fr"
+          templateColumns="110px 110px 110px"
+        >
+          <GridItem area="categoryIcon">
+            <Box>
+              <CategoryIcon category={post.category} />
+            </Box>
+          </GridItem>
+          <GridItem area="category" my={3} mx={6}>
+            <Box>
+              <CategoryTag category={post.category} />
+            </Box>
+          </GridItem>
+          <GridItem area="title" mx={6}>
+            <Box
+              mt="2"
+              overflow="hidden"
+              display="-webkit-box"
+              style={{
+                WebkitLineClamp: 1,
+                WebkitBoxOrient: "vertical"
+              }}
+              height="20px"
+            >
+              <Heading size="md" isTruncated height="20px">
+                {post.title}
+              </Heading>
+            </Box>
+          </GridItem>
+          <GridItem area="description" my={3} mx={6}>
+            <Box
+              mt="2"
+              color="gray.600"
+              overflow="hidden"
+              display="-webkit-box"
+              style={{
+                WebkitLineClamp: 2,
+                WebkitBoxOrient: "vertical"
+              }}
+              height="50px"
+            >
+              {post.description}
+            </Box>
+          </GridItem>
+          <GridItem area="read"my={2} mx={6}>
             <Flex alignItems="center">
               <Box as="span" fontWeight="bold" mr="2">
                 <FaBookReader />
               </Box>
               <Box as="span">{post.read}</Box>
             </Flex>
+          </GridItem>
+          <GridItem area="like" my={2} mx={6}>
             <Flex alignItems="center">
               <Box as="span" fontWeight="bold" mr={2}>
                 <FaHeart color="pink" />
               </Box>
               <Box as="span">{post.favorites}</Box>
             </Flex>
+          </GridItem>
+          <GridItem area="views" my={2} mx={6}>
             <Flex alignItems="center">
               <Box as="span" fontWeight="bold" mr={2}>
                 <FaEye />
               </Box>
               <Box as="span">{post.views}</Box>
             </Flex>
-          </Flex>
-        </Box>
+          </GridItem>
+        </Grid>
       </Box>
     </Box>
   );
