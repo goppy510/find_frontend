974258e6d9981b44518e430d80fbf2c8d9555f0d
goppy <tacumin.goppy.06@gmail.com>
2023-04-21 03:41:49 +0900

    fix

--- frontend/app/src/features/main/Main.tsx
+++ frontend/app/src/features/main/Main.tsx
@@ -2,13 +2,7 @@ import { Container } from  "@/features/components";
 
 export default function Main({ children }: { children: React.ReactNode }) {
   return (
-    <Container
-      as="main"
-      maxW="8xl"
-      px={14}
-      py={3}
-      marginTop="60px"
-    >
+    <Container as="main">
       {children}
     </Container>
   );
