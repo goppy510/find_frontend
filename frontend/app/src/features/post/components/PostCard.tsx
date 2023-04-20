c5be9faefaf013365be86e20828fab9407873b86
goppy <tacumin.goppy.06@gmail.com>
2023-04-20 13:41:40 +0900

    カードのスタイルを調整した

--- frontend/app/src/features/post/components/PostCard.tsx
+++ frontend/app/src/features/post/components/PostCard.tsx
@@ -3,10 +3,15 @@
 import {
   Box,
   Flex,
-  Heading,
-  Text
+  Heading
 } from "@/features/components";
+import {
+  FaHeart,
+  FaEye,
+  FaBookReader
+} from 'react-icons/fa';
 import CategoryIcon from "@/features/components/CategoryIcon";
+import CategoryTag from "@/features/components/CategoryTag";
 import { Post } from "@/features/post/types/post_types";
 
 type PostCardProps = {
@@ -14,29 +19,68 @@ type PostCardProps = {
 };
 
 export default function PostCard({ post }: PostCardProps) {
+
   return (
-    <Box borderWidth="1px" borderRadius="lg" overflow="hidden" margin={5} width="350px" >
+    <Box borderWidth="1px" borderRadius="lg" overflow="hidden" margin={5} width="350px">
       <Box as="a" href={`/posts/${post.id}`} cursor="pointer">
-          <Box position="relative" width="100%" height="200px">
-            <CategoryIcon category={post.category} />
-          </Box>
-          <Box p="6">
-            <Flex alignItems="baseline">
-              <Text fontWeight="semibold" color="gray.600" fontSize="sm">
-                {post.category}
-              </Text>
-            </Flex>
+        <Box position="relative" width="100%" height="120px">
+          <CategoryIcon category={post.category} />
+        </Box>
+        <Box p="6">
+          <Flex alignItems="baseline" mb={2}>
+            <CategoryTag category={post.category} />
+          </Flex>
 
-            <Flex mt="2" justify="space-between" align="center">
-              <Heading size="md" isTruncated maxH="3em">
-                {post.title}
-              </Heading>
-            </Flex>
-
-            <Text mt="2" color="gray.600">
-              {post.description}
-            </Text>
+          <Box
+            mt="2"
+            overflow="hidden"
+            display="-webkit-box"
+            style={{
+              WebkitLineClamp: 1,
+              WebkitBoxOrient: "vertical"
+            }}
+            height="20px"
+          >
+            <Heading size="md" isTruncated height="20px">
+              {post.title}
+            </Heading>
           </Box>
+
+          <Box
+            mt="2"
+            color="gray.600"
+            overflow="hidden"
+            display="-webkit-box"
+            style={{
+              WebkitLineClamp: 2,
+              WebkitBoxOrient: "vertical"
+            }}
+            height="50px"
+          >
+            {post.description}
+          </Box>
+
+          <Flex mt={4} justifyContent="space-between" alignItems="center">
+            <Flex alignItems="center">
+              <Box as="span" fontWeight="bold" mr="2">
+                <FaBookReader />
+              </Box>
+              <Box as="span">{post.read}</Box>
+            </Flex>
+            <Flex alignItems="center">
+              <Box as="span" fontWeight="bold" mr={2}>
+                <FaHeart color="pink" />
+              </Box>
+              <Box as="span">{post.favorites}</Box>
+            </Flex>
+            <Flex alignItems="center">
+              <Box as="span" fontWeight="bold" mr={2}>
+                <FaEye />
+              </Box>
+              <Box as="span">{post.views}</Box>
+            </Flex>
+          </Flex>
+        </Box>
       </Box>
     </Box>
   );
