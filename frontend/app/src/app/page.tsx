c9c9d646a46d73596bf808ec8c7f3a87138ab75b
goppy <tacumin.goppy.06@gmail.com>
2023-04-20 13:41:55 +0900

    mock変更

--- frontend/app/src/app/page.tsx
+++ frontend/app/src/app/page.tsx
@@ -7,39 +7,69 @@ import SideBar from "@/features/sidebar/SideBar";
 const posts: Post[] = [
   {
     id: 1,
-    title: "投稿タイトル1",
+    title: "Ultimate Marketing Audience",
     description: "投稿本文1",
-    category: "IT"
+    category: "IT",
+    favorites: 120,
+    views: 280,
+    read: 50,
+    creatorName: 'hogehoge',
+    creatorIcon: "https://github.com/identicons/pronama.png"
   },
   {
     id: 2,
-    title: "投稿タイトル2",
-    description: "投稿本文2",
-    category: "会計・経理・税務"
+    title: "Midjourney Prompts Helperdddddd",
+    description: "With this Midjourney Prompts helper, you can enter your predefined Midjourney Prompts, and ChatGPT will refine your ",
+    category: "会計・経理・税務",
+    favorites: 120,
+    views: 280,
+    read: 50,
+    creatorName: 'hogehoge',
+    creatorIcon: "https://github.com/identicons/pronama.png"
   },
   {
     id: 3,
     title: "投稿タイトル3",
     description: "投稿本文3",
-    category: "法務"
+    category: "法務",
+    favorites: 120,
+    views: 280,
+    read: 50,
+    creatorName: 'hogehoge',
+    creatorIcon: "https://github.com/identicons/pronama.png"
   },
   {
     id: 4,
     title: "投稿タイトル4",
     description: "投稿本文4",
-    category: "エクセル・スプレッドシート"
+    category: "エクセル・スプレッドシート",
+    favorites: 120,
+    views: 280,
+    read: 50,
+    creatorName: 'hogehoge',
+    creatorIcon: "https://github.com/identicons/pronama.png"
   },
   {
     id: 5,
     title: "投稿タイトル5",
     description: "投稿本文5",
-    category: "文書"
+    category: "文書",
+    favorites: 120,
+    views: 280,
+    read: 50,
+    creatorName: 'hogehoge',
+    creatorIcon: "https://github.com/identicons/pronama.png"
   },
   {
     id: 6,
     title: "投稿タイトル6",
     description: "投稿本文6",
-    category: "メール"
+    category: "メール",
+    favorites: 120,
+    views: 280,
+    read: 50,
+    creatorName: 'hogehoge',
+    creatorIcon: "https://github.com/identicons/pronama.png"
   },
   // その他の投稿データ
 ];
