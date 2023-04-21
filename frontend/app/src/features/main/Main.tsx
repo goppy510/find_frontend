89f5acd1394cd7f4fc0112abdc613f66b61b535e
goppy <tacumin.goppy.06@gmail.com>
2023-04-22 01:27:15 +0900

    container 余白調整

--- frontend/app/src/features/main/Main.tsx
+++ frontend/app/src/features/main/Main.tsx
@@ -2,7 +2,7 @@ import { Container } from  "@/features/components";
 
 export default function Main({ children }: { children: React.ReactNode }) {
   return (
-    <Container as="main">
+    <Container as="main" maxW="unset" px={0}>
       {children}
     </Container>
   );
