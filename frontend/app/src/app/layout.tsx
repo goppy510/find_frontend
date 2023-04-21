e2225a66a5fa900226e0f5f083949f2a32ac8612
goppy <tacumin.goppy.06@gmail.com>
2023-04-21 23:18:23 +0900

    layoutにhtmlタグ等をつけてホットリロードするように修正した

--- frontend/app/src/app/layout.tsx
+++ frontend/app/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import Provider from "@/features/components/Provider"
 import Header from "@/features/header/Header"
+import SideBar from '@/features/sidebar/SideBar'
 import Main from "@/features/main/Main"
 import Footer from "@/features/footer/Footer"
 import {
@@ -10,32 +11,39 @@ import {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <Provider>
-      <Grid
-        templateAreas={`
-          "header header header"
-          "nav top space"
-          "nav main space"
-          "footer footer footer"
-        `}
-        templateRows="50px 50px 1fr 30px"
-        templateColumns="250px 1fr 1fr"
-        h="100vh"
-        w="100%"
-        gap={2}
-      >
-        <GridItem area="header">
-          <Header />
-        </GridItem>
-        <GridItem area="nav">Nav</GridItem>
-        <GridItem area="top">Top</GridItem>
-        <GridItem area="main">
-          <Main>{children}</Main>
-        </GridItem>
-        <GridItem area="footer">
-          <Footer />
-        </GridItem>
-      </Grid>
-    </Provider>
+    <html lang="ja">
+      <head />
+      <body>
+        <Provider>
+          <Grid
+            templateAreas={`
+              "header header header"
+              "sideBar top space"
+              "sideBar main space"
+              "footer footer footer"
+            `}
+            templateRows="50px 50px 1fr 30px"
+            templateColumns="250px 1fr 1fr"
+            h="100vh"
+            w="100%"
+            gap={2}
+          >
+            <GridItem area="header">
+              <Header />
+            </GridItem>
+            <GridItem area="sideBar">
+              <SideBar />
+            </GridItem>
+            <GridItem area="top">Top</GridItem>
+            <GridItem area="main">
+              <Main>{children}</Main>
+            </GridItem>
+            <GridItem area="footer">
+              <Footer />
+            </GridItem>
+          </Grid>
+        </Provider>
+      </body>
+    </html>
   );
 }
