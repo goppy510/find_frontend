84ea726477bea12dcdd398f1d414d936dc940320
goppy <tacumin.goppy.06@gmail.com>
2023-04-25 13:27:31 +0900

    change

--- frontend/app/src/features/post/components/PostCard/PostCard.tsx
+++ frontend/app/src/features/post/components/PostCard/PostCard.tsx
@@ -1,20 +1,19 @@
-// @ts-nocheck
-// use client
+"use client";
 import {
   Box,
-  Flex,
-  Heading,
   Grid,
   GridItem
 } from "@/features/components";
 import {
-  FaEye,
-  FaBookReader,
-  FaThumbsUp
-} from 'react-icons/fa';
-import CategoryIcon from "@/features/components/CategoryIcon";
-import CategoryTag from "@/features/components/CategoryTag";
-import { Post } from "@/features/post/types/post_types";
+  CategoryIcon,
+  CategoryTag,
+  Title,
+  Description,
+  MemberViews,
+  Likes,
+  Views
+} from "@/features/post/components/PostCard/parts";
+import { Post } from "@/features/post/types/PostTypes";
 
 type PostCardProps = {
   post: Post;
@@ -38,75 +37,31 @@ export default function PostCard({ post }: PostCardProps) {
             "category category category"
             "title title title"
             "description description description"
-            "read like views"
+            "memberViews likes views"
           `}
           templateRows="120px 1fr 20px 70px 1fr"
           templateColumns="110px 110px 110px"
         >
           <GridItem area="categoryIcon">
-            <Box height="120px">
-              <CategoryIcon category={post.category} />
-            </Box>
+            <CategoryIcon category={post.category} />
           </GridItem>
           <GridItem area="category" my={3} mx={6}>
-            <Box>
-              <CategoryTag category={post.category} />
-            </Box>
+            <CategoryTag category={post.category} />
           </GridItem>
           <GridItem area="title" mx={6}>
-            <Box
-              mt="2"
-              overflow="hidden"
-              display="-webkit-box"
-              style={{
-                WebkitLineClamp: 1,
-                WebkitBoxOrient: "vertical"
-              }}
-              height="20px"
-            >
-              <Heading size="md" isTruncated height="20px">
-                {post.title}
-              </Heading>
-            </Box>
+            <Title title={post.title} />
           </GridItem>
           <GridItem area="description" my={3} mx={6}>
-            <Box
-              mt="2"
-              color="gray.600"
-              overflow="hidden"
-              display="-webkit-box"
-              style={{
-                WebkitLineClamp: 2,
-                WebkitBoxOrient: "vertical"
-              }}
-              height="50px"
-            >
-              {post.description}
-            </Box>
+            <Description description={post.description} />
           </GridItem>
-          <GridItem area="read"my={2} mx={6}>
-            <Flex alignItems="center">
-              <Box as="span" fontWeight="bold" mr="2">
-                <FaBookReader />
-              </Box>
-              <Box as="span">{post.read}</Box>
-            </Flex>
+          <GridItem area="memberViews"my={2} mx={6}>
+            <MemberViews memberViews={post.memberViews} />
           </GridItem>
-          <GridItem area="like" my={2} mx={6}>
-            <Flex alignItems="center">
-              <Box as="span" fontWeight="bold" mr={2}>
-                <FaThumbsUp />
-              </Box>
-              <Box as="span">{post.favorites}</Box>
-            </Flex>
+          <GridItem area="likes" my={2} mx={6}>
+            <Likes likes={post.likes} />
           </GridItem>
           <GridItem area="views" my={2} mx={6}>
-            <Flex alignItems="center">
-              <Box as="span" fontWeight="bold" mr={2}>
-                <FaEye />
-              </Box>
-              <Box as="span">{post.views}</Box>
-            </Flex>
+            <Views views={post.views} />
           </GridItem>
         </Grid>
       </Box>
