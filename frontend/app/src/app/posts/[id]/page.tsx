2d50e43d93bf81c8c2f31ad658c3e689c69273c9
goppy <tacumin.goppy.06@gmail.com>
2023-04-25 13:33:53 +0900

    change

--- frontend/app/src/app/posts/[id]/page.tsx
+++ frontend/app/src/app/posts/[id]/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Box } from "@/features/components";
-import PostCardDetail from "@/features/post/components/PostCardDetail";
-import { PostId } from "@/features/post/types/post_types";
+import PostCardDetail from "@/features/post/components/PostCardDetail/PostCardDetail";
+import { PostId } from "@/features/post/types/PostTypes";
 
 export default function PostPage({ params }: { params: { id: PostId } }) {
 
@@ -11,22 +11,3 @@ export default function PostPage({ params }: { params: { id: PostId } }) {
     </Box>
   );
 }
-
-// export const getStaticPaths: GetStaticPaths = async () => {
-//   // すべての投稿のIDを取得する
-//   const posts = await fetch("https://jsonplaceholder.typicode.com/posts").then((res) =>
-//     res.json()
-//   );
-//   const postIds = posts.map((post: any) => post.id);
-
-//   // paramsに渡すIDの配列を作成する
-//   const paths = postIds.map((id) => ({ params: { id: id.toString() } }));
-
-//   return { paths, fallback: true };
-// };
-
-// export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
-//   const post = await getPostById(params?.id as string);
-//   return { props: { post } };
-// };
-
