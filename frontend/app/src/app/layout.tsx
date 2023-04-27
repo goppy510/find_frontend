84ff46273e0b3b6c49dc527bfc8ffcb0d44da3f1
goppy <tacumin.goppy.06@gmail.com>
2023-04-28 03:38:09 +0900

    pxを廃止

--- frontend/app/src/app/layout.tsx
+++ frontend/app/src/app/layout.tsx
@@ -5,10 +5,7 @@ import Top from '@/features/top/Top'
 import SideBar from '@/features/sidebar/SideBar'
 import Main from "@/features/main/Main"
 import Footer from "@/features/footer/Footer"
-import {
-  Grid,
-  GridItem
-} from "@/features/components";
+import { Grid, GridItem } from "@/features/components";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -23,10 +20,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
               "sideBar main space"
               "footer footer footer"
             `}
-            templateRows="50px 120px 1fr 30px"
-            templateColumns="2fr 7fr 1fr"
-            h="100vh"
-            w="100%"
+            templateRows="0.5fr 1fr 10fr 0.5fr"
+            templateColumns="2fr 8fr 1fr"
             gap={2}
           >
             <GridItem area="header">
