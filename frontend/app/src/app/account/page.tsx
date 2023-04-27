427c4bda01aea95c0e3a0bcecaeb83fcb2df6256
goppy <tacumin.goppy.06@gmail.com>
2023-04-28 03:37:50 +0900

    change

--- frontend/app/src/app/account/page.tsx
+++ frontend/app/src/app/account/page.tsx
@@ -1,15 +1,14 @@
-import { Flex, Box} from "@/features/components";
-import { AccountType } from "@/features/account/types/account_types";
+import { AccountType } from "@/features/account/types/account-types";
 import AccountPage from "@/features/account/AccountPage";
 
 // 投稿データを定義する mock
 const account: AccountType = {
     id: 1,
-    creatorName: 'Segun Adebayo',
-    creatorIcon: "https://bit.ly/sage-adebayo",
-    like: 120,
+    accountName: 'Segun Adebayo',
+    avatar: "https://bit.ly/sage-adebayo",
+    likes: 120,
     views: 280,
-    read: 50
+    memberViews: 50
   }
 
 export default function Account({
