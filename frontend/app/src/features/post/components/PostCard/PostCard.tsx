8bf2ee59b1f81dd1ed5bce97297d743a822f4218
goppy <tacumin.goppy.06@gmail.com>
2023-04-28 03:39:01 +0900

    change

--- frontend/app/src/features/post/components/PostCard/PostCard.tsx
+++ frontend/app/src/features/post/components/PostCard/PostCard.tsx
@@ -26,9 +26,9 @@ export default function PostCard({ post }: PostCardProps) {
       borderWidth="1px"
       borderRadius="lg"
       overflow="hidden"
-      w="100%"
+      w="330px"
       boxShadow="xl"
-      _hover={{ bg: "gray.50" }}
+      _hover={{ bg: "gray.100" }}
     >
       <Box as="a" href={`/posts/${post.id}`} cursor="pointer">
         <Grid
