71d95e3f913a9e5d3cb84dfb05e193e86c31d82a
goppy <tacumin.goppy.06@gmail.com>
2023-09-15 07:03:10 +0900

    change

--- app/src/app/profile/mypage/page.tsx
+++ app/src/app/profile/mypage/page.tsx
@@ -1,22 +1,48 @@
-import { AccountType } from "@/features/account/types/account-types";
-import AccountPage from "@/features/account/AccountPage";
+'use client';
+import { MyPageType } from "@/features/profile/types/myPageTypes";
+import MyPage from '@/features/profile/components/MyPage';
+import apiClient from "@/lib/api-client";
+import { useState, useEffect } from "react";
 
-// 投稿データを定義する mock
-const account: AccountType = {
-    id: 1,
-    accountName: 'Segun Adebayo',
-    avatar: "https://bit.ly/sage-adebayo",
-    likes: 120,
-    views: 280,
-    memberViews: 50
-  }
+export default function Account({ children, } : { children: React.ReactNode }) {
+  const [profile, setProfile] = useState<MyPageType | null>(null);
+  const [errorMessage, setErrorMessage] = useState('');
 
-export default function Account({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+  useEffect(() => {
+    const handleFetchProfile = async () => {
+      try {
+        const endpoint = '/api/users/profile'
+        const response = await apiClient.get(endpoint);
+
+        if (response.status === 200) {
+            // ここでAPIから取得したデータを MyPageType 型として設定します
+            const profile: MyPageType = {
+              userId: response.data.user_id,
+              name: response.data.name,
+              phoneNumber: response.data.phone_number,
+              companyName: response.data.company_name,
+              employeeCount: response.data.employee_count,
+              industry: response.data.industry,
+              position: response.data.position,
+              businessModel: response.data.business_model
+            };
+            setProfile(profile);
+        } else {
+          setErrorMessage('プロフィールの読み込みに失敗しました。');
+        }
+      } catch (error) {
+        console.error('Fetch Profile Failed:', error);
+        setErrorMessage('プロフィールの読み込みに失敗しました。');
+      }
+    };
+
+    handleFetchProfile();
+  }, []);
+
+    // プロフィールデータがnullの場合、何も表示しないか、ローディングを表示するなどの処理が可能です
+    if (!profile) return null;
+
   return (
-      <AccountPage account={account} />
+      <MyPage profile={profile} />
   );
 }
