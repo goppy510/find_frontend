485e3f7790bf24e7cbff80f1ce58e762eb35fbb8
goppy <tacumin.goppy.06@gmail.com>
2023-09-14 21:45:53 +0900

    change

--- app/next.config.js
+++ app/next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  exlint: {
+    dirs: ['src',]
+  },
   experimental: {
     appDir: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
