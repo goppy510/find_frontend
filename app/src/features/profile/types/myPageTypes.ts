e3c33995e6cd57ff36a10382a3f6d1db9f240d44
goppy <tacumin.goppy.06@gmail.com>
2023-09-15 07:02:34 +0900

    change

--- app/src/features/profile/types/myPageTypes.ts
+++ app/src/features/profile/types/myPageTypes.ts
@@ -1,9 +1,11 @@
 // アカウントに表示する内容
 export interface MyPageType {
-  id: number;
-  accountName: string;
-  avatar: string;
-  likes: number;
-  views: number;
-  memberViews: number;
+  userId: number;
+  name: string;
+  phoneNumber: string;
+  companyName: string;
+  employeeCount: string;
+  industry: string;
+  position: string;
+  businessModel: string;
 }
