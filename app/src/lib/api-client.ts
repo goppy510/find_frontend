5d241492e95a7a7e93e4bd18142c645f07da517b
goppy <tacumin.goppy.06@gmail.com>
2023-09-14 23:22:36 +0900

    cahnge

--- app/src/lib/api-client.ts
+++ app/src/lib/api-client.ts
@@ -1,26 +1,36 @@
-import axios from "axios";
+import axios from 'axios';
 
-const baseURL = process.env.MAKELEAD_APP_API_ENDPOINT || 'http://localhost:3000';
+const baseURL =
+  process.env.MAKELEAD_APP_API_ENDPOINT || 'http://localhost:3000';
 
 // axios の初期設定
 // 例：　await apiClient.get('/endopoint')
 const apiClient = axios.create({
   baseURL,
+  withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
+    'X-Requested-With': 'XMLHttpRequest',
   },
 });
 
 // リクエスト時にトークンをヘッダーに含める
-apiClient.interceptors.request.use((config) => {
-  const csrfToken = getCookie('csrf-token');  // CSRFトークンを取得するための関数
-  if (csrfToken) {
-    config.headers['X-CSRF-Token'] = csrfToken;
+apiClient.interceptors.request.use(
+  (config) => {
+    const csrfToken = getCookie('csrf-token'); // CSRFトークンを取得するための関数
+    if (csrfToken) {
+      config.headers['X-CSRF-Token'] = csrfToken;
+    }
+    const token = localStorage.getItem('jwtToken');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
+);
 
 // レスポンスのエラー判定処理
 apiClient.interceptors.response.use(
@@ -54,6 +64,6 @@ const getCookie = (name: string): string | undefined => {
   const value = `; ${document.cookie}`;
   const parts = value.split(`; ${name}=`);
   if (parts.length === 2) return parts.pop()?.split(';').shift();
-}
+};
 
 export default apiClient;
