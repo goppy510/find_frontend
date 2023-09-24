3cf1cd5db3f34fdcf846e91e692204570b967638
goppy <tacumin.goppy.06@gmail.com>
2023-09-25 01:37:56 +0900

    change

--- app/src/features/prompt/components/CategoryTag.tsx
+++ app/src/features/prompt/components/CategoryTag.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { Box } from "@chakra-ui/react";
-import CategoryTagMapping from "@/features/card/components/CategoryTagMapping";
+import CategoryTagMapping from "@/features/prompt/components/CategoryTagMapping";
 
 type CategroyProps = {
   category: string;
