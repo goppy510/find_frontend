3879cf283685ffae62f7aec021b2306c2359cb65
goppy <tacumin.goppy.06@gmail.com>
2023-09-25 22:39:35 +0900

    add

---
+++ app/src/features/promptDetail/components/InputExample.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Box, Flex } from "@chakra-ui/react";
+
+type Props = {
+  text: string;
+};
+
+export default function InputExample({text}: Props) {
+  const lines = text.split('\n');
+  const height = `${2 + lines.length * 1.5}em`;
+
+  return (
+    <Box w={{ base: '100%' }} minH={{ base: height }} bg="blue.900" color="white">
+      <Flex justifyContent="space-between" alignItems="center">
+        <Box mx="auto">
+          <Box fontSize="1xl" mb="4" mx="3">
+            {lines.map((line, index) => (
+              <React.Fragment key={index}>
+                {line}
+                <br />
+              </React.Fragment>
+            ))}
+          </Box>
+        </Box>
+      </Flex>
+    </Box>
+  );
+};  
