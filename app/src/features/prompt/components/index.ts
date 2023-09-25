34493edb46637a1637c6295359be54de4b43bb34
goppy <tacumin.goppy.06@gmail.com>
2023-09-25 15:54:19 +0900

    add

--- app/src/features/prompt/components/index.ts
+++ app/src/features/prompt/components/index.ts
@@ -1,3 +1,4 @@
+import GenerativeAuModel from './GenerativeAiModel';
 export { default as CategoryIcon } from './CategoryIcon';
 export { default as CategoryTag } from './CategoryTag';
 export { default as Title } from './Title';
@@ -5,3 +6,4 @@ export { default as About } from './About';
 export { default as BookmarksCount } from './BookmarksCount';
 export { default as LikesCount } from './LikesCount';
 export { default as Views } from './Views';
+export { default as GenerativeAiModel } from './GenerativeAiModel';
