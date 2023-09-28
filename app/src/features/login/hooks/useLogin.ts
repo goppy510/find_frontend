dc827dbc67a610677ac0ee234a3eec80ac4e72fe
goppy <tacumin.goppy.06@gmail.com>
2023-09-29 00:57:52 +0900

    login hooks

---
+++ app/src/features/login/hooks/useLogin.ts
@@ -0,0 +1,36 @@
+// /hooks/useLogin.ts
+import { useState } from 'react';
+import apiClient from '@/lib/api-client';
+
+const useLogin = () => {
+  const [errorMessage, setErrorMessage] = useState<string>('');
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const resetMessages = () => {
+    setErrorMessage('');
+  };
+
+  const handleLogin = async (email: string, password: string) => {
+    resetMessages();
+    try {
+      setIsLoading(true);
+      const endpoint = '/api/users/login';
+      const params = { login: { email: email, password: password } };
+      const response = await apiClient.post(endpoint, params);
+      if (response.status === 200) {
+        localStorage.setItem('jwtToken', response.data.token);
+        window.location.href = '/';
+      } else {
+        setErrorMessage('ログインに失敗しました。');
+      }
+    } catch (error) {
+      setErrorMessage('ログインに失敗しました。');
+    } finally {
+      setIsLoading(false); // 成功または失敗後にisLoadingをfalseに設定
+    }
+  };
+
+  return { handleLogin, errorMessage, isLoading };
+};
+
+export default useLogin;
