4744380f16319377d54d115e65af8a3d2006fc42
goppy <tacumin.goppy.06@gmail.com>
2024-01-09 00:14:30 +0900

    add

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -3,12 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'airbnb',
-    'airbnb/hooks',
-    'airbnb-typescript',
-    'prettier',
-  ],
+  extends: ['airbnb', 'airbnb/hooks', 'airbnb-typescript', 'prettier', 'plugin:storybook/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
