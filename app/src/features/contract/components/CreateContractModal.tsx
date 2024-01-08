934604077bfd0301ea603ebf7e92aee39bc42796
goppy <tacumin.goppy.06@gmail.com>
2024-01-08 18:22:10 +0900

    change

---
+++ app/src/features/contract/components/CreateContractModal.tsx
@@ -0,0 +1,115 @@
+import React, { useState, useEffect } from 'react';
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  Checkbox,
+} from '@chakra-ui/react';
+
+interface CreateContractModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onCreate: (email: string, password: string, maxMemberCount: string) => void;
+}
+
+const CreateContractModal: React.FC<CreateContractModalProps> = ({
+  isOpen,
+  onClose,
+  onCreate,
+}) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [maxMemberCount, setMaxMembers] = useState('');
+  const [isFill, setIsFill] = useState(false);
+
+  useEffect(() => {
+    const numericMaxMembers = maxMemberCount ? parseInt(maxMemberCount, 10) : 0;
+    if (email && password === confirmPassword && numericMaxMembers > 0) {
+      setIsFill(true);
+    } else {
+      setIsFill(false);
+    }
+  }, [email, password, confirmPassword, maxMemberCount]); // 入力値または確認テキストが変わった時に一致をチェック
+
+  const handleSubmit = () => {
+    if (password === confirmPassword) {
+      onCreate(email, password, maxMemberCount);
+      onClose();
+    } else {
+      // Handle error: passwords do not match or terms not agreed
+    }
+  };
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>契約作成</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody pb={6}>
+          <FormControl isRequired>
+            <FormLabel>メールアドレス</FormLabel>
+            <Input
+              placeholder="example@a.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </FormControl>
+
+          <FormControl isRequired>
+            <FormLabel>パスワード</FormLabel>
+            <Input
+              placeholder="P@ssw0rd"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </FormControl>
+
+          <FormControl isRequired>
+            <FormLabel>パスワード確認</FormLabel>
+            <Input
+              placeholder="Confirm Password"
+              type="password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </FormControl>
+
+          <FormControl isRequired>
+            <FormLabel>最大契約メンバー数</FormLabel>
+            <Input
+              placeholder="5"
+              type="number"
+              value={maxMemberCount}
+              onChange={(e) => setMaxMembers(e.target.value)}
+            />
+          </FormControl>
+        </ModalBody>
+
+        <ModalFooter>
+          <Button
+            colorScheme="blue"
+            mr={3}
+            onClick={handleSubmit}
+            isDisabled={!isFill}
+          >
+            契約作成
+          </Button>
+          <Button onClick={onClose}>キャンセル</Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default CreateContractModal;
---
+++ app/src/features/contract/hooks/useCreateContracts.ts
@@ -0,0 +1,96 @@
+import { useState } from 'react';
+import apiClient from '@/lib/api-client';
+import axios from 'axios';
+
+const useCreateContract = () => {
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [duplicateErrorMessage, setDuplicateErrorMessage] = useState('');
+  const [unprocessibleEntityErrorMessage, setUnprocessibleEntityErrorMessage] =
+    useState('');
+  const [successMessage, setSuccessMessage] = useState('');
+  const [isChecked, setIsChecked] = useState(false);
+  const [isCreated, setIsCreated] = useState(false);
+
+  const resetMessages = () => {
+    setErrorMessage('');
+    setSuccessMessage('');
+    setDuplicateErrorMessage('');
+    setUnprocessibleEntityErrorMessage('');
+  };
+
+  const handleSignup = async (
+    email: string,
+    password: string,
+    max_member_count: string
+  ) => {
+    resetMessages();
+    try {
+      const endpoint = '/api/users/signup';
+      const params = {
+        signups: {
+          email: email,
+          password: password,
+          max_member_count: Number(max_member_count),
+        },
+      };
+      const response = await apiClient.post(endpoint, params);
+
+      if (response.status === 200) {
+        setSuccessMessage('契約を作成しました');
+        setIsCreated(true);
+      } else {
+        setErrorMessage('会員登録に失敗しました');
+      }
+    } catch (error) {
+      if (error instanceof axios.AxiosError) {
+        // ここで error は AxiosError 型として扱われます
+        if (error.response && error.response.status === 409) {
+          setDuplicateErrorMessage('このメールアドレスは既に登録されています');
+        } else if (
+          error.response &&
+          error.response.status === 422 &&
+          error.response.data.error.code === 'SignupService::EmailFormatError'
+        ) {
+          setUnprocessibleEntityErrorMessage(
+            'メールアドレスの形式が正しくありません'
+          );
+        } else if (
+          error.response &&
+          error.response.status === 422 &&
+          error.response.data.error.code ===
+            'SignupService::PasswordFormatError'
+        ) {
+          setUnprocessibleEntityErrorMessage(
+            'パスワードの形式が正しくありません'
+          );
+        } else {
+          setErrorMessage('会員登録に失敗しました');
+        }
+      } else {
+        setErrorMessage('会員登録に失敗しました');
+      }
+    } finally {
+      setIsLoading(false); // 成功または失敗後にisLoadingをfalseに設定
+    }
+  };
+
+  return {
+    confirmPassword,
+    setConfirmPassword,
+    isLoading,
+    setIsLoading,
+    errorMessage,
+    duplicateErrorMessage,
+    unprocessibleEntityErrorMessage,
+    successMessage,
+    isChecked,
+    setIsChecked,
+    handleSignup,
+    isCreated,
+    setIsCreated,
+  };
+};
+
+export default useCreateContract;
