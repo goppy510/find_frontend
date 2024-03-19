c8a066de4e926887590d2a658f9f39e240f6576e
goppy <tacumin.goppy.06@gmail.com>
2024-03-19 23:22:53 +0900

    change

--- app/src/features/permission/types/permissionTypes.ts
+++ app/src/features/permission/types/permissionTypes.ts
@@ -7,13 +7,16 @@ export interface Permission {
 }
 
 export const permissionMap = {
-  プロンプト読み取り: 'read_prompt',
+  管理者: 'admin',
+  契約者: 'contract',
+  権限: 'permission',
+  プロンプト閲覧: 'read_prompt',
   プロンプト作成: 'create_prompt',
   プロンプト更新: 'update_prompt',
   プロンプト削除: 'destroy_prompt',
   ユーザー作成: 'user',
-  権限付与: 'permission',
 };
 
 // TypeScriptの型定義でマップのキーを制限
 export type PermissionKey = keyof typeof permissionMap;
+export type PermissionValue = (typeof permissionMap)[PermissionKey];
