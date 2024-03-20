befd3fab6cc99916c5425748d8b648cb9170a55d
goppy <tacumin.goppy.06@gmail.com>
2024-03-20 11:54:58 +0900

    fix

--- app/src/features/permission/hooks/useDeletePermissions.ts
+++ app/src/features/permission/hooks/useDeletePermissions.ts
@@ -7,11 +7,12 @@ const useDeletePermissions = () => {
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
   const [successMessage, setSuccessMessage] = useState<string | null>(null);
 
-  const handleDelete = async (email: string) => {
+  const handleDelete = async (user_id: string) => {
     setIsLoading(true);
     setErrorMessage(null);
+    const endpoint = `/api/permissions/${user_id}`;
     try {
-      await apiClient.delete(`/api/permissions/${email}`);
+      await apiClient.delete(endpoint);
       setSuccessMessage('権限データを削除しました。');
     } catch (error) {
       setErrorMessage('権限データの削除中にエラーが発生しました。');
@@ -20,7 +21,14 @@ const useDeletePermissions = () => {
     }
   };
 
-  return { handleDelete, isLoading, errorMessage, successMessage };
+  return {
+    handleDelete,
+    isLoading,
+    errorMessage,
+    successMessage,
+    setSuccessMessage,
+    setErrorMessage,
+  };
 };
 
 export default useDeletePermissions;
