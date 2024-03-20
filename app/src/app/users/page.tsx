c3035131e602996aa505858e82a1cc8d0fcf5f5a
goppy <tacumin.goppy.06@gmail.com>
2024-03-20 13:39:40 +0900

    fix

--- app/src/app/users/page.tsx
+++ app/src/app/users/page.tsx
@@ -12,103 +12,65 @@ import {
   Box,
   Flex,
 } from '@chakra-ui/react';
-import { FaPen, FaTrashAlt, FaCheckSquare, FaRegSquare } from 'react-icons/fa';
+import { FaPen, FaTrashAlt } from 'react-icons/fa';
 import Loading from '@/components/elements/loading/Loading';
 import SuccessToast from '@/components/elements/toast/SuccessToast';
 import ErrorToast from '@/components/elements/toast/ErrorToast';
-import {
-  Permission,
-  permissionMap,
-} from '@/features/permission/types/permissionTypes';
-import CreatePermissionModal from '@/features/permission/components/CreatePermissionModal';
-import EditPermissionModal from '@/features/permission/components/EditPermissionModal';
-import DeletePermissionModal from '@/components/elements/deleteModal/DeleteModal';
-import useFetchPermissions from '@/features/permission/hooks/useFetchPermissions';
-import useCreatePermission from '@/features/permission/hooks/useCreatePermissions';
-import useEditPermission from '@/features/permission/hooks/useEditPermissions';
-import useDeletePermission from '@/features/permission/hooks/useDeletePermissions';
-import { set } from 'react-hook-form';
+import { User } from '@/features/user/types/userTypes';
+import CreateUserModal from '@/features/user/components/CreateUserModal';
+import DeleteUserModal from '@/components/elements/deleteModal/DeleteModal';
+import useFetchUsers from '@/features/user/hooks/useFetchUsers';
+import useCreateUser from '@/features/user/hooks/useCreateUser';
+import useDeleteUser from '@/features/user/hooks/useDeleteUser';
 
-export default function Users() {
+export default function Contracts() {
   const localStorageUserId = localStorage.getItem('userId');
   const [isDeleteModalOpen, setDeleteModalOpen] = useState(false);
   const [isCreateModalOpen, setCreateModalOpen] = useState(false);
   const [isEditModalOpen, setEditModalOpen] = useState(false);
-  const [selectedPermission, setSelectedPermission] = useState<
-    Permission | undefined
-  >(undefined);
+  const [selectedUser, setSelectedUser] = useState<User | undefined>(undefined);
 
+  // 一覧取得API
   const {
-    permissions,
-    setPermissions,
+    users,
+    setUsers,
     isLoading: fetchIsLoading,
     errorMessage: fetchErrorMessage,
     handleFetch,
-  } = useFetchPermissions();
+  } = useFetchUsers();
 
+  // 契約作成API
   const {
     errorMessage: createErrorMessage,
+    duplicateErrorMessage,
+    unprocessibleEntityErrorMessage,
     successMessage: createSuccessMessage,
-    handleCreate,
+    handleSignup,
     isCreated,
     setIsCreated,
     setSuccessMessage: setCreateSuccessMessage,
     setErrorMessage: setCreateErrorMessage,
-  } = useCreatePermission();
+  } = useCreateUser();
 
+  // 削除API
   const {
-    errorMessage: editErrorMessage,
-    successMessage: editSuccessMessage,
-    isLoading: editIsLoading,
-    handleUpdate,
-    isEdited,
-    setIsEdited,
-    setSuccessMessage: setEditSuccessMessage,
-    setErrorMessage: setEditErrorMessage,
-  } = useEditPermission();
-
-  const {
+    handleDelete,
+    isLoading: deleteIsLoading,
     errorMessage: deleteErrorMessage,
     successMessage: deleteSuccessMessage,
-    isLoading: deleteIsLoading,
-    handleDelete,
     setSuccessMessage: setDeleteSuccessMessage,
     setErrorMessage: setDeleteErrorMessage,
-  } = useDeletePermission();
+  } = useDeleteUser();
 
   const [justDeleted, setJustDeleted] = useState(false);
-  const reversePermissionMap = Object.entries(permissionMap).reduce(
-    (acc, [jp, en]) => {
-      acc[en] = jp;
-      return acc;
-    },
-    {} as { [key: string]: string }
-  );
-
-  const checkedPermissionsJp = (userPermissions: string[]) => {
-    // APIから取得したユーザーの権限名の配列を逆マッピングして日本語に変換
-    return userPermissions.map((en) => reversePermissionMap[en] || en);
-  };
-
-  const calculateCheckedPermissions = (userPermissions: string[]) => {
-    // すべての権限に対するチェック状態のマップを作成
-    const allPermissionsCheckedState: { [key: string]: boolean } = {};
-    Object.keys(permissionMap).forEach((jp) => {
-      allPermissionsCheckedState[jp] =
-        checkedPermissionsJp(userPermissions).includes(jp);
-    });
-    return allPermissionsCheckedState;
-  };
 
   // 削除操作
   const handleDeleteConfirm = async (email: string) => {
-    if (selectedPermission && selectedPermission.email === email) {
+    if (selectedUser && selectedUser.email === email) {
       try {
-        await handleDelete(selectedPermission.user_id.toString());
-        setPermissions((prevPermissions) =>
-          prevPermissions.filter(
-            (p) => p.user_id !== selectedPermission.user_id
-          )
+        await handleDelete(selectedUser.user_id);
+        setUsers((prevUsers) =>
+          prevUsers.filter((u) => u.user_id !== selectedUser.user_id)
         );
         setDeleteModalOpen(false); // モーダルを閉じる
       } catch (error) {
@@ -127,45 +89,30 @@ export default function Users() {
     }
   }, [isCreated, setIsCreated]);
 
-  // 編集後の処理
-  useEffect(() => {
-    if (isEdited) {
-      handleFetch();
-      setIsEdited(false);
-      setEditSuccessMessage('');
-      setEditErrorMessage('');
-    }
-  }, [isEdited, setIsEdited]);
-
   // 削除後の処理
   useEffect(() => {
     if (justDeleted) {
-      const updatedPermissions = permissions.filter(
-        (p) => p.email !== selectedPermission?.email
+      // リストを更新
+      const updatedUsers = users.filter(
+        (u) => u.user_id !== selectedUser?.user_id
       );
-      setPermissions(updatedPermissions);
+      setUsers(updatedUsers);
       setJustDeleted(false);
-      setSelectedPermission(undefined);
+      setSelectedUser(undefined);
       setDeleteSuccessMessage('');
       setDeleteErrorMessage('');
     }
-  }, [justDeleted, selectedPermission, permissions, setPermissions]);
-
-  // 削除モーダルを閉じる
-  const handleCloseDeleteModal = () => {
-    setDeleteModalOpen(false);
-    setSelectedPermission(undefined);
-  };
-
-  // 編集モーダルを閉じる
-  const handleCloseEditModal = () => {
-    setEditModalOpen(false);
-    setSelectedPermission(undefined);
-  };
+  }, [justDeleted, selectedUser, users, setUsers]);
 
   // 作成モーダルを閉じる
   const handleCloseCreateModal = () => {
     setCreateModalOpen(false);
+  };
+
+  // 削除モーダルを閉じる
+  const handleCloseDeleteModal = () => {
+    setDeleteModalOpen(false);
+    setSelectedUser(undefined);
   };
 
   if (fetchIsLoading || deleteIsLoading) {
@@ -176,48 +123,36 @@ export default function Users() {
     <>
       {deleteSuccessMessage && <SuccessToast message={deleteSuccessMessage} />}
       {createSuccessMessage && <SuccessToast message={createSuccessMessage} />}
-      {editSuccessMessage && <SuccessToast message={editSuccessMessage} />}
+      {duplicateErrorMessage && <ErrorToast message={duplicateErrorMessage} />}
+      {unprocessibleEntityErrorMessage && (
+        <ErrorToast message={unprocessibleEntityErrorMessage} />
+      )}
       {deleteErrorMessage && <ErrorToast message={deleteErrorMessage} />}
       {createErrorMessage && <ErrorToast message={createErrorMessage} />}
-      {editErrorMessage && <ErrorToast message={editErrorMessage} />}
       {fetchErrorMessage && <ErrorToast message={fetchErrorMessage} />}
 
-      <CreatePermissionModal
+      <CreateUserModal
         isOpen={isCreateModalOpen}
         onClose={handleCloseCreateModal}
-        onCreate={(email, permissions) => {
-          handleCreate(email, permissions);
+        onCreate={(email, password) => {
+          handleSignup(email, password);
         }}
       />
 
-      {selectedPermission && (
-        <EditPermissionModal
-          isOpen={isEditModalOpen}
-          onClose={handleCloseEditModal}
-          onEdit={(permissions) =>
-            handleUpdate(selectedPermission.user_id.toString(), permissions)
-          }
-          email={selectedPermission.email}
-          selectedPermissions={checkedPermissionsJp(
-            selectedPermission.permissions
-          )}
-        />
-      )}
-
-      {selectedPermission && (
-        <DeletePermissionModal
+      {selectedUser && (
+        <DeleteUserModal
           isOpen={isDeleteModalOpen}
           onClose={handleCloseDeleteModal}
-          onDelete={() => handleDeleteConfirm(selectedPermission.email)}
-          confirmText={selectedPermission.email}
-          placeholder={selectedPermission.email}
+          onDelete={() => handleDeleteConfirm(selectedUser.email)}
+          confirmText={selectedUser.email}
+          placeholder={selectedUser.email}
         />
       )}
 
       <Box mb={4} overflow="hidden">
         <Flex justifyContent="flex-end">
           <Button colorScheme="blue" onClick={() => setCreateModalOpen(true)}>
-            権限作成
+            メンバー作成
           </Button>
         </Flex>
       </Box>
@@ -228,57 +163,36 @@ export default function Users() {
             <Th>契約ID</Th>
             <Th>ユーザーID</Th>
             <Th>email</Th>
-            <Th>権限</Th>
+            <Th>アクティベーション</Th>
+            <Th>データ作成日時</Th>
+            <Th>データ更新日</Th>
             <Th>操作</Th>
           </Tr>
         </Thead>
         <Tbody>
-          {permissions?.map((permission) => {
-            const checkedPermissions = calculateCheckedPermissions(
-              permission.permissions
-            );
-            return (
-              <Tr key={permission.user_id}>
-                <Td>{permission.contract_id}</Td>
-                <Td>{permission.user_id}</Td>
-                <Td>{permission.email}</Td>
-                <Td>
-                  {Object.keys(permissionMap).map((jp) => (
-                    <Box key={jp} display="flex" alignItems="center">
-                      {checkedPermissions[jp] ? (
-                        <FaCheckSquare />
-                      ) : (
-                        <FaRegSquare />
-                      )}
-                      <Box ml={2}>{jp}</Box>
-                    </Box>
-                  ))}
-                </Td>
-                <Td>
+          {users.map((user) => (
+            <Tr key={user.contract_id}>
+              <Td>{user.contract_id}</Td>
+              <Td>{user.user_id}</Td>
+              <Td>{user.email}</Td>
+              <Td>{user.activated ? '済' : '未'}</Td>
+              <Td>{new Date(user.created_at).toLocaleString()}</Td>
+              <Td>{new Date(user.updated_at).toLocaleString()}</Td>
+              <Td>
+                {localStorageUserId !== user.user_id.toString() && (
                   <IconButton
-                    aria-label="編集"
-                    icon={<FaPen />}
+                    aria-label="削除"
+                    icon={<FaTrashAlt />}
+                    colorScheme="red"
                     onClick={() => {
-                      setSelectedPermission(permission);
-                      setEditModalOpen(true);
+                      setSelectedUser(user);
+                      setDeleteModalOpen(true);
                     }}
-                    mr={2}
                   />
-                  {localStorageUserId !== permission.user_id.toString() && (
-                    <IconButton
-                      aria-label="削除"
-                      icon={<FaTrashAlt />}
-                      colorScheme="red"
-                      onClick={() => {
-                        setSelectedPermission(permission);
-                        setDeleteModalOpen(true);
-                      }}
-                    />
-                  )}
-                </Td>
-              </Tr>
-            );
-          })}
+                )}
+              </Td>
+            </Tr>
+          ))}
         </Tbody>
       </Table>
     </>
