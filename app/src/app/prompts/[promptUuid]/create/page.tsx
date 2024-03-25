f7de8f4ec83589c8ae1fe67929566e0a7f06a9e3
goppy <tacumin.goppy.06@gmail.com>
2024-03-25 16:56:06 +0900

    不要コード削除

---
+++ app/src/app/prompts/[promptUuid]/create/page.tsx
@@ -0,0 +1,11 @@
+import PromptForm from "@/features/promptForm/components/PromptForm"
+
+export default function Post({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+      <PromptForm />
+  );
+}
