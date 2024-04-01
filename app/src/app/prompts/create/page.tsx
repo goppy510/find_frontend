affe7507603ef2f19ba58bb6341dfef33818280c
goppy <tacumin.goppy.06@gmail.com>
2024-04-01 10:19:26 +0900

    change

---
+++ app/src/app/prompts/create/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+import React from 'react';
+import PromptForm from '@/features/promptForm/components/PromptForm';
+
+export default function Post({ children }: { children: React.ReactNode }) {
+  return <PromptForm />;
+}
