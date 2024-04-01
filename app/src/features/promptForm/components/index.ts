e890ab87913942a6d16af2d804e4c31608bcba30
goppy <tacumin.goppy.06@gmail.com>
2024-04-01 10:21:53 +0900

    fix

--- app/src/features/promptForm/components/index.ts
+++ app/src/features/promptForm/components/index.ts
@@ -1,6 +1,7 @@
-export { default as Model } from "./Model";
-export { default as Title } from "./Title";
-export { default as Description } from "./Description";
-export { default as Prompt } from "./Prompt";
-export { default as ExampleInput } from "./ExampleInput";
-export { default as ExampleOutput } from "./ExampleOutput";
+export { default as Model } from './Model';
+export { default as Title } from './Title';
+export { default as Category } from './Category';
+export { default as Description } from './Description';
+export { default as Prompt } from './Prompt';
+export { default as ExampleInput } from './InputExample';
+export { default as ExampleOutput } from './OutputExample';
