b9fd013f1370b2676ef8d53f61cdbbce8c390f31
goppy <tacumin.goppy.06@gmail.com>
2024-04-02 09:50:20 +0900

    プロフィール画像不要なので削除

--- app/src/features/promptDetail/components/Creator.tsx
+++ app/src/features/promptDetail/components/Creator.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { Box } from "@chakra-ui/react";
-import { CreatorType } from "@/features/promptDetail/types/creatorTypes";
+import { Box } from '@chakra-ui/react';
+import { CreatorType } from '@/features/promptDetail/types/creatorTypes';
 
 export default function Creator({ userLink, nickname }: CreatorType) {
   return (
     <Box as="a" href={userLink} display="flex" alignItems="center">
-      <Box w="32px" h="32px" borderRadius="50%" overflow="hidden">
-        <Box as="img" src="" w="100%" h="100%" objectFit="cover" />
-      </Box>
       <Box ml="2">
-        <Box as="span" fontWeight="bold" _hover={{ textDecoration: "underline" }}>
+        <Box
+          as="span"
+          fontWeight="bold"
+          _hover={{ textDecoration: 'underline' }}
+        >
           {nickname}
         </Box>
       </Box>
