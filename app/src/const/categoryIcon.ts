0bffb0e65e0519f0123674e573b2307a709b321a
goppy <tacumin.goppy.06@gmail.com>
2024-04-02 22:30:12 +0900

    アイコン変えた

--- app/src/const/categoryIcon.ts
+++ app/src/const/categoryIcon.ts
@@ -1,29 +1,23 @@
 import { CategoryIconDetails } from '@/types/categoryIcon';
 import {
-  FaBus,
-  FaMoneyBillWave,
+  FaWrench,
+  FaStore,
   FaLaptopCode,
   FaBuilding,
-  FaHospital,
-  FaBook,
-  FaPlane,
-  FaFilm,
+  FaPeopleArrows,
   FaBullhorn,
   FaHandshake,
   FaEllipsisH,
 } from 'react-icons/fa';
 
 const details: CategoryIconDetails = {
-  'IT・情報通信業': { icon: FaLaptopCode, bg: 'green.300' },
-  '金融・保険業': { icon: FaMoneyBillWave, bg: 'yellow.300' },
-  不動産業: { icon: FaBuilding, bg: 'orange.300' },
-  '交通・運輸業': { icon: FaBus, bg: 'blue.300' }, // バスのアイコンを選択していますが、他の交通手段を主にする場合は変更してください。
-  '医療・福祉': { icon: FaHospital, bg: 'pink.300' },
-  '教育・学習支援業': { icon: FaBook, bg: 'teal.300' },
-  '旅行・宿泊・飲食業': { icon: FaPlane, bg: 'purple.300' }, // 旅行を主として選択していますが、飲食を強調したい場合は変更してください。
-  'エンターテインメント・マスコミ': { icon: FaFilm, bg: 'blue.300' },
-  '広告・マーケティング': { icon: FaBullhorn, bg: 'teal.300' },
-  コンサルティング業: { icon: FaHandshake, bg: 'gray.300' },
+  Webサービス: { icon: FaLaptopCode, bg: 'green.300' },
+  不動産: { icon: FaBuilding, bg: 'orange.300' },
+  メーカー: { icon: FaWrench, bg: 'blue.300' },
+  店舗運営: { icon: FaStore, bg: 'purple.300' },
+  人材: { icon: FaPeopleArrows, bg: 'pink.300' },
+  '広告・Web制作・マーケティング支援': { icon: FaBullhorn, bg: 'teal.300' },
+  セールスプロモーション: { icon: FaHandshake, bg: 'yellow.300' },
   その他: { icon: FaEllipsisH, bg: 'gray.300' },
 };
 
--- app/src/const/categoryTag.ts
+++ app/src/const/categoryTag.ts
@@ -1,16 +1,13 @@
 import { CategoryTagDetails } from '@/types/categoryTag';
 
 const details: CategoryTagDetails = {
-  'IT・情報通信業': { colorScheme: 'green' },
-  '金融・保険業': { colorScheme: 'yellow' },
-  不動産業: { colorScheme: 'orange' },
-  '交通・運輸業': { colorScheme: 'blue' },
-  '医療・福祉': { colorScheme: 'pink' },
-  '教育・学習支援業': { colorScheme: 'teal' },
-  '旅行・宿泊・飲食業': { colorScheme: 'purple' },
-  'エンターテインメント・マスコミ': { colorScheme: 'blue' }, // 前回の"店舗運営"と同じ色ですが、異なる色を希望する場合は変更してください。
-  '広告・マーケティング': { colorScheme: 'teal' }, // 一致するものを選びました。
-  コンサルティング業: { colorScheme: 'gray' }, // 一般的な業種なのでgrayを選択しました。変更しても問題ありません。
+  Webサービス: { colorScheme: 'green' },
+  セールスプロモーション: { colorScheme: 'yellow' },
+  不動産: { colorScheme: 'orange' },
+  メーカー: { colorScheme: 'blue' },
+  人材: { colorScheme: 'pink' },
+  '広告・Web制作・マーケティング支援': { colorScheme: 'teal' },
+  店舗運営: { colorScheme: 'purple' },
   その他: { colorScheme: 'gray' },
 };
 
